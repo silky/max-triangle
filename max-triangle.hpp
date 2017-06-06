3b68d055a845799f7a7b1cbcf55a459b27fe9128
Yoav Kallus <yoav@santafe.edu>
2017-06-06 16:39:35 -0600

    started

---
+++ max-triangle.hpp
@@ -0,0 +1,18 @@
+#include <gmpxx.h>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <string>
+
+enum state_flag {status_ok, status_not_convex, status_no_interior, status_runtime_error};
+
+struct state {
+    mpq_class nx, ny;
+    int ai,bi,ci;
+    mpq_class bt, ct;
+    state_flag status;
+};
+
+bool is_convex(std::vector<mpq_class> polygon);
+struct state anchored_triangle(std::vector<mpq_class> polygon, mpq_class nx, mpq_class ny);
+struct state maximum_triangle(std::vector<mpq_class> polygon);
