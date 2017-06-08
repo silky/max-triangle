16da6324883743ad8965a7c88c25e5a738d0b1a9
Yoav Kallus <yoav@santafe.edu>
2017-06-08 09:33:44 -0600

    clean up

--- max-triangle.cpp
+++ max-triangle.cpp
@@ -1,9 +1,6 @@
 #include <iomanip>
 #include "max-triangle.hpp"
 
-//#define getmod(arg1,arg2) arg1[(((arg2)+(arg1.size()))%(arg1.size()))]
-//#define getmod(arg1,arg2) arg1[((arg2)%(arg1.size()))]
-
 void print_status(state_flag status) {
     switch(status) {
 	case status_ok: std::cout << "status_ok" << std::endl; break;
@@ -32,128 +29,11 @@ bool is_convex(std::vector<mpq_class> polygon) {
 
 }
 
-/*struct state anchored_triangle(std::vector<mpq_class> polygon, mpq_class nx, mpq_class ny){
-
-    struct state my_state;
-    mpq_class minx,maxx,x;
-    mpq_class ax,ay,bx,by,cx,cy;
-    mpq_class ebx,eby,ecx,ecy;
-    mpq_class dbt, dct, t0, tq;
-    mpq_class ee, neb, nec;
-    unsigned int i;
-
-    if (polygon.size() < 3) { my_state.status = status_no_interior; return my_state; }
-    if (!is_convex(polygon)) { my_state.status = status_not_convex; return my_state; }
-    my_state.status = status_ok;
-
-    maxx = minx = nx*polygon[0]+ny*polygon[1];
-    my_state.ai = my_state.ci = 0;
-    for (i = 1; i < polygon.size()/2; i++) {
-	x = nx*polygon[2*i+0]+ny*polygon[2*i+1];
-	if (x>maxx) { maxx=x; my_state.ci=i; }
-	if (x<minx) { minx=x; my_state.ai=i; }
-    }
-    my_state.bi=my_state.ci-1;
-    my_state.bt=1;
-    my_state.ct=0;
-
-    ax = polygon[(2*my_state.ai+0)%(2*sz)];
-    ay = polygon[(2*my_state.ai+1)%(2*sz)];
-    cx = polygon[(2*my_state.ci+0)%(2*sz)];
-    cy = polygon[(2*my_state.ci+1)%(2*sz)];
-    bx = cx;
-    by = cy;
-    ebx = polygon[(2*my_state.bi+2)%(2*sz)] - polygon[(2*my_state.bi+0)%(2*sz)];
-    eby = polygon[(2*my_state.bi+3)%(2*sz)] - polygon[(2*my_state.bi+1)%(2*sz)];
-    ecx = polygon[(2*my_state.ci+2)%(2*sz)] - polygon[(2*my_state.ci+0)%(2*sz)];
-    ecy = polygon[(2*my_state.ci+3)%(2*sz)] - polygon[(2*my_state.ci+1)%(2*sz)];
-
-    while(true) {
-	neb = ebx*nx + eby*ny; 
-	nec = ecx*nx + ecy*ny;
-	ee = ebx*ecy - ecx*eby;
-	if (neb < 0) { my_state.status = status_runtime_error; return my_state; }
-	if (nec > 0) { my_state.status = status_runtime_error; return my_state; }
-
-	if (neb == 0) { //eb.n = 0, advance b
-	    my_state.bi -= 1; 
-	    my_state.bt = 1;
-	    bx = polygon[(2*my_state.bi+2)%(2*sz)];
-	    by = polygon[(2*my_state.bi+3)%(2*sz)];
-	    ebx = polygon[(2*my_state.bi+2)%(2*sz)] - polygon[(2*my_state.bi+0)%(2*sz)];
-	    eby = polygon[(2*my_state.bi+3)%(2*sz)] - polygon[(2*my_state.bi+1)%(2*sz)];
-	    continue;
-	}
-	if (nec == 0) { //ec.n = 0, advance c
-	    my_state.ci += 1; 
-	    my_state.ct = 0;
-	    cx = polygon[(2*my_state.ci+0)%(2*sz)];
-	    cy = polygon[(2*my_state.ci+1)%(2*sz)];
-	    ecx = polygon[(2*my_state.ci+2)%(2*sz)] - polygon[(2*my_state.ci+0)%(2*sz)];
-	    ecy = polygon[(2*my_state.ci+3)%(2*sz)] - polygon[(2*my_state.ci+1)%(2*sz)];
-	    continue;
-	}
-	if (ee <= 0) { //eb and ec are parallel or converging, any advancement reduces area, so done
-	    return my_state;
-	}
-
-	//dbt  = nx*eby*ecx*(ax-bx) + nx*ecy*ebx*(2*bx-ax-cx);
-	//dbt += ny*ebx*ecy*(by-ay) + ny*ecx*eby*(ay+cy-2*by);
-	//dbt /= 2*(ebx*ecy - ecx*eby)*(ebx*nx + eby*ny);
-
-	//dct  = nx*ecy*ebx*(ax-cx) + nx*eby*ecx*(2*cx-ax-bx);
-	//dct += ny*ecx*eby*(cy-ay) + ny*ebx*ecy*(ay+by-2*cy);
-	//dct /= 2*(ebx*ecy - ecx*eby)*(ecx*nx + ecy*ny);
-	
-	tq  = nx*(by*ebx*ecx - cy*ebx*ecx - ax*eby*ecx + cx*eby*ecx + ax*ebx*ecy - bx*ebx*ecy);
-	tq -= ny*(ay*eby*ecx - by*eby*ecx - ay*ebx*ecy + cy*ebx*ecy + bx*eby*ecy - cx*eby*ecy);
-
-	dbt = -tq/(2*neb*ee);
-	dct = tq/(2*nec*ee);
-
-	//std::cout << my_state.bi << ", " << my_state.ci << ", " << my_state.bt << ", " << my_state.ct << ", " << dbt << ", " << dct << ", " << bx << ", " << by << ", " << cx << ", " << cy << std::endl;
-
-	if (dbt <= 0 || dct <= 0) return my_state;
-
-	if (dbt < my_state.bt && dct < 1 - my_state.ct) {
-	    my_state.bt -= dbt;
-	    my_state.ct += dct;
-	    return my_state;
-	}
-
-	if ( (1 - my_state.ct)*dbt < my_state.bt*dct ) {
-	    t0 = (1 - my_state.ct)*dbt/dct;
-	    bx -= t0*ebx;
-	    by -= t0*eby;
-	    my_state.bt -= t0;
-	    my_state.ci += 1; 
-	    my_state.ct = 0;
-	    cx = polygon[(2*my_state.ci+0)%(2*sz)];
-	    cy = polygon[(2*my_state.ci+1)%(2*sz)];
-	    ecx = polygon[(2*my_state.ci+2)%(2*sz)] - polygon[(2*my_state.ci+0)%(2*sz)];
-	    ecy = polygon[(2*my_state.ci+3)%(2*sz)] - polygon[(2*my_state.ci+1)%(2*sz)];
-	} else {
-	    t0 = my_state.bt*dct/dbt;
-	    cx += t0*ecx;
-	    cy += t0*ecy;
-	    my_state.ct += t0;
-	    my_state.bi -= 1; 
-	    my_state.bt = 1;
-	    bx = polygon[(2*my_state.bi+2)%(2*sz)];
-	    by = polygon[(2*my_state.bi+3)%(2*sz)];
-	    ebx = polygon[(2*my_state.bi+2)%(2*sz)] - polygon[(2*my_state.bi+0)%(2*sz)];
-	    eby = polygon[(2*my_state.bi+3)%(2*sz)] - polygon[(2*my_state.bi+1)%(2*sz)];
-	}
-    }
-}*/
-
 void maximum_triangle(std::vector<mpq_class> polygon, unsigned int *ret, state_flag *status){
 
     mpq_class nx, ny;
     unsigned int ai,bi,ci;
     mpq_class bt, ct;
-    //state_flag status;
-    //int ret[3] = {0,0,0};
     unsigned int sz = polygon.size()/2;
     bool max_at_vert=false;
     unsigned int iter = 0;
@@ -168,18 +48,11 @@ void maximum_triangle(std::vector<mpq_class> polygon, unsigned int *ret, state_f
     mpq_class area;
     mpq_class amax = -1;
 
-    /*my_state = anchored_triangle(polygon,polygon[1]-polygon[3],polygon[2]-polygon[0]);
-    nx = polygon[1]-polygon[3];
-    ny = polygon[2]-polygon[0];
-    if (status != status_ok) {return; }
-    ai_start = ai = 0;*/
-
-    ////////////////////////////
     if (polygon.size() < 3) { *status = status_no_interior; return;  }
     if (!is_convex(polygon)) { *status = status_not_convex; return;  }
     *status = status_ok;
 
-    bi = 1;
+    bi = 0;
     ci = bi+1;
     ai = bi+2;
     bt = 0;
@@ -195,8 +68,6 @@ void maximum_triangle(std::vector<mpq_class> polygon, unsigned int *ret, state_f
 
     ai_start = ai;
     
-    /////////////////////////////
-
     ax = polygon[(2*ai+0)%(2*sz)];
     ay = polygon[(2*ai+1)%(2*sz)];
     bx = polygon[(2*bi+0)%(2*sz)];
@@ -431,17 +302,9 @@ void maximum_triangle(std::vector<mpq_class> polygon, unsigned int *ret, state_f
 	    std::cout << "t3c: " << std::scientific << std::setfill(' ') << std::scientific << std::setw(10) << std::setprecision(3) << t3c.get_d() << std::endl;
 #endif
 
-	    //time in terms of b's motion until (1 or 2 or 3)
-	    /*if (t2 < t1) t1 = t2;
-	    if (t3 < t1 && t3 > 0) t1 = t3;
-	    if (t1 < 0) {*status=status_runtime_error; return; }*/
-	    
-	    //if (t2b <= t1b && t2c <= t1c) {t1b = t2b; t1c = t2c;}
 	    if ( (t1c < 0) || (t2b <= t1b && t2c <= t1c) ) { t1c = t2c; t1b = t2b;}
 	    if (t3b <= t1b && t3c <= t1c && t3b >= 0 && t3c >= 0) {t1b = t3b; t1c = t3c;}
 	    if (t1b < 0 || t1c < 0) {*status=status_runtime_error; return; }
-	    //time in terms of c's motion until (1 or 2 or 3)
-	    //t2 = (ay*ebx + by*ebx - 2*cy*ebx - ax*eby - bx*eby + 2*cx*eby)*t1/(ay*ecx - 2*by*ecx + cy*ecx - ax*ecy + 2*bx*ecy - cx*ecy + 2*ee*t1);
 
 	    bx += t1b*ebx;
 	    by += t1b*eby;
@@ -455,9 +318,6 @@ void maximum_triangle(std::vector<mpq_class> polygon, unsigned int *ret, state_f
     }
 
     if (!max_at_vert) *status = status_max_not_at_vertex;
-    //ai = ret[0];
-    //bi = ret[1];
-    //ci = ret[2];
     return; 
 }
 
--- max-triangle.hpp
+++ max-triangle.hpp
@@ -5,16 +5,7 @@
 #include <string>
 
 enum state_flag {status_ok, status_not_convex, status_no_interior, status_runtime_error, status_max_not_at_vertex, status_maxiter_exceeded};
-
-/*struct state {
-    mpq_class nx, ny;
-    int ai,bi,ci;
-    mpq_class bt, ct;
-    state_flag status;
-};*/
-
 void print_status(state_flag status);
 bool is_convex(std::vector<mpq_class> polygon);
-//struct state anchored_triangle(std::vector<mpq_class> polygon, mpq_class nx, mpq_class ny);
 void maximum_triangle(std::vector<mpq_class> polygon, unsigned int *ret, state_flag *status);
 void brute_force_maximum_triangle(std::vector<mpq_class> polygon, unsigned int *ret, state_flag *status);
